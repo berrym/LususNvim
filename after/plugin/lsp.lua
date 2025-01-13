a4bfdd21b1dd8a0814aef34f11af0d5bf1c39bb9
Michael Berry <trismegustis@gmail.com>
2025-01-12 22:59:18 -0500

    LSP configuration change.
    Reconfigure lsp-zero and cmp removing lsp attach autocommand.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,13 @@
-local lsp = require("lsp-zero")
-lsp.preset("minimal")
+local lsp = require("lsp-zero").preset({
+  name = "recommended",
+  manage_nvim_cmp = {
+    set_extra_mappings = true,
+  },
+})
+
+lsp.on_attach(function(_, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
+end)
 
 lsp.set_sign_icons({
   error = "✘",
@@ -9,18 +17,7 @@ lsp.set_sign_icons({
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
 local exist, custom_config = pcall(require, "custom.custom_config")
-lsp.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
-  servers = custom_config.formatting_servers,
-})
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
@@ -28,7 +25,7 @@ require("mason-lspconfig").setup({
     function(server_name)
       capabilities = require("cmp_nvim_lsp").default_capabilities()
       require("lspconfig")[server_name].setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
     end,
   },
@@ -43,21 +40,57 @@ require("mason-lspconfig").setup({
     end,
   },
 })
+
 lsp.setup()
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local bufnr = args.buf
-    local opts = {buffer = bufnr, remap = false}
-    vim.keymap.set("n","gd",function() vim.lsp.buf.definition() end, opts) --go to definition
-    vim.keymap.set('n','K',function() vim.lsp.buf.hover() end, opts) -- hover
-    vim.keymap.set('n','<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts) --view workspace
-    vim.keymap.set('n','<leader>vd', function() vim.diagnostic.open_foat() end, opts) --view diagnostic
-    vim.keymap.set('n','[d',function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n',']d',function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n','<leader>vca', function() vim.lsp.buf.code_action() end, opts) --view code action
-    vim.keymap.set('n','<leader>vrn', function() vim.lsp.buf.rename() end, opts) --rename variables
-    vim.keymap.set('n','<leader>vrr', function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n','<leader>h', function() vim.lsp.buf.signature_help() end, opts)
-  end
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
+local lspkind = require("lspkind")
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "nvim_lua" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  preselect = "item",
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+  },
+  mapping = {
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<Tab>"] = cmp_action.luasnip_supertab(),
+    ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol", -- show only symbol annotations
+      maxwidth = {
+        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+        menu = 50, -- leading text (labelDetails)
+        abbr = 50, -- actual suggestion item
+      },
+      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(_, vim_item)
+        -- ...
+        return vim_item
+      end,
+    }),
+  },
 })
