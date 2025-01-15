ec8c432e040069dbbec8f76909d7e69b59c2d286
Michael Berry <trismegustis@gmail.com>
2025-01-15 15:38:42 -0500

    Use noice cmdline_popup, turn off smartcase and ignorecase options.

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -1,31 +1,31 @@
-local ok, _ = pcall(require, "notify")
 require("noice").setup({
-  messages = {
-    view = "mini",
-    view_search = "mini",
+  presets = {
+    bottom_search = true,
+    command_palette = false,
+    long_message_to_split = true,
+    lsp_doc_border = true,
   },
-  routes = {
-    {
-      view = ok and "notify" or "mini",
-      filter = {
-        event = "msg_show",
-        find = "substitutions",
+  views = {
+    cmdline_popup = {
+      position = {
+        row = "5",
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = "auto",
       },
     },
-    { filter = { find = "fewer lines;" }, opts = { skip = true } },
-    { filter = { find = "more line;" }, opts = { skip = true } },
-    { filter = { find = "more lines;" }, opts = { skip = true } },
-    { filter = { find = "less;" }, opts = { skip = true } },
-    { filter = { find = "change;" }, opts = { skip = true } },
-    { filter = { find = "changes;" }, opts = { skip = true } },
-    { filter = { find = "indent" }, opts = { skip = true } },
-    { filter = { find = "move" }, opts = { skip = true } },
-    { filter = { find = "No information available" }, opts = { skip = true } },
-  },
-  cmdline = { view = "cmdline" },
-  views = {
     popupmenu = {
-      size = { width = 50, height = 10 },
+      relative = "editor",
+      position = {
+        row = "8",
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = "auto",
+      },
       border = {
         style = "rounded",
         padding = { 0, 1 },
@@ -34,20 +34,5 @@ require("noice").setup({
         winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
       },
     },
-  },
-  lsp = {
-    message = {
-      enabled = false,
-    },
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-  },
-  presets = {
-    bottom_search = true,
-    long_message_to_split = true,
-    lsp_doc_border = true,
   },
 })
