e27ef0d922720b902420843418706b46e3fcdf40
Michael Berry <trismegustis@gmail.com>
2025-02-18 03:28:31 -0500

    Change blink.cmp settings
    Enable ghost text, disable cmdline.

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -72,7 +72,6 @@ require("blink.cmp").setup({
     --   'select_next'
     -- },
   },
-
   appearance = {
     -- Sets the fallback highlight groups to nvim-cmp's highlight groups
     -- Useful for when your theme doesn't support blink.cmp
@@ -80,19 +79,20 @@ require("blink.cmp").setup({
     use_nvim_cmp_as_default = true,
     -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
     -- Adjusts spacing to ensure icons are aligned
-    nerd_font_variant = "mono",
+    nerd_font_variant = "normal",
   },
-
   completion = {
     list = { selection = { preselect = true, auto_insert = true } },
+    ghost_text = {
+      enabled = true,
+      -- Show the ghost text when an item has been selected
+      show_with_selection = true,
+      -- Show the ghost text when no item has been selected, defaulting to the first item
+      show_without_selection = false,
+    },
   },
-
-  signature = { enabled = true },
-
-  -- Default list of enabled providers defined so that you can extend it
-  -- elsewhere in your config, without redefining it, due to `opts_extend`
-  sources = {
-    default = { "lsp", "path", "snippets", "buffer" },
+  cmdline = {
+    enabled = false,
   },
 })
 
