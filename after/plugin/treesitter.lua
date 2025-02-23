f0561cb328a6deb93a6c665327c2bcb643dcdb19
Michael Berry <trismegustis@gmail.com>
2025-02-23 00:22:17 -0500

    Reconfigure treesitter setup.
    Automatically install parsers from custum.custom_config group
    treesitter_ensure_installed.
    Refresh treesitter-context setup.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,11 +1,17 @@
 local exist, custom_config = pcall(require, "custom.custom_config")
 local group = exist and type(custom_config) == "table" and custom_config.enable_plugins or {}
+local parsers = exist
+    and type(custom_config) == "table"
+    and custom_config.treesitter_ensure_installed
+    or {}
 local enabled = require("config.utils").enabled
 local autotag = enabled(group, "autopairs")
 local rainbow = enabled(group, "rainbow")
 
 if enabled(group, "treesitter") then
   require("nvim-treesitter.configs").setup({
+    auto_install = true,
+    ensure_installed = parsers,
     highlight = {
       enable = true,
     },
