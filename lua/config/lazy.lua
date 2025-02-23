d6bc6dfa13c038bc4704c62ce51d5dec9e3669aa
Michael Berry <trismegustis@gmail.com>
2025-02-22 21:57:48 -0500

    Change dap config.
    setup nvim-dap-virtual-text outside of config.lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -182,9 +182,6 @@ local plugins = {
       },
       {
         "theHamsta/nvim-dap-virtual-text",
-        config = function()
-          require("nvim-dap-virtual-text").setup()
-        end,
       },
       {
         "nvim-neotest/nvim-nio",
