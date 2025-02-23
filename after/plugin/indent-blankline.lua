d6bc00d55a06ce3a79b9624a50d659c6aafac08f
Michael Berry <trismegustis@gmail.com>
2025-02-23 13:26:46 -0500

    Add more user commands to control some UI aspects.
    User commands to toggle ibl colored indents and tokyonight colorscheme.

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -5,7 +5,7 @@ local enabled = require("config.utils").enabled
 if enabled(group, "indent_blankline") then
   if enabled(group, "indent_blankline_rainbow") then
     -- configure multiple indent colors for indent-blankline
-    local highlight = {
+    _G.ibl_rainbow_highlight = {
       "RainbowRed",
       "RainbowGreen",
       "RainbowOrange",
@@ -14,10 +14,10 @@ if enabled(group, "indent_blankline") then
       "RainbowViolet",
       "RainbowCyan",
     }
-    local hooks = require("ibl.hooks")
+    _G.ibl_hooks = require("ibl.hooks")
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    _G.ibl_hooks.register(_G.ibl_hooks.type.HIGHLIGHT_SETUP, function()
       vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
       vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
       vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
@@ -27,7 +27,7 @@ if enabled(group, "indent_blankline") then
       vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
     -- end configuring multiple indent colors
-    require("ibl").setup({ indent = { highlight = highlight } })
+    require("ibl").setup({ indent = { highlight = _G.ibl_rainbow_highlight } })
   else
     require("ibl").setup()
   end
--- lua/config/usercommands.lua
+++ lua/config/usercommands.lua
@@ -1,17 +1,28 @@
-vim.api.nvim_create_user_command("LususUpdate", function()
+local create_user_command = vim.api.nvim_create_user_command
+local colors = require("config.utils").colors
+
+create_user_command("LususUpdate", function()
   require("config.utils").update_all()
 end, { desc = "Updates plugins, mason packages, treesitter parsers" })
 
-vim.api.nvim_create_user_command("AstroTransparencyOn", function()
+create_user_command("AstroTransparencyOn", function()
   require("astrotheme").setup({
     style = { transparent = true },
   })
-  require("config.utils").colors("astromars")
+  colors("astromars")
 end, { desc = "Enable astrotheme transparency" })
 
-vim.api.nvim_create_user_command("AstroTransparencyOff", function()
+create_user_command("AstroTransparencyOff", function()
   require("astrotheme").setup({
     style = { transparent = false },
   })
-  require("config.utils").colors("astromars")
+  colors("astromars")
 end, { desc = "Disable astrotheme transparency" })
+
+create_user_command("IblRainbowOn", function()
+  require("ibl").setup({ indent = { highlight = _G.ibl_rainbow_highlight } })
+end, { desc = "Enable colored indent markers" })
+
+create_user_command("IblRainbowOff", function()
+  require("ibl").setup()
+end, { desc = "Disable colored indent markers" })
