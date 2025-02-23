18b0ca1eaf48565e8caf5d4e47e7b8568675c592
Michael Berry <trismegustis@gmail.com>
2025-02-23 07:47:56 -0500

    Rename astrotheme transparency control commands.
    AstrothemeTransparentEnable -> AstroTransparencyOn
    AstrothemeTransparentDisable -> AstroTransparencyOff

--- lua/config/usercommands.lua
+++ lua/config/usercommands.lua
@@ -2,14 +2,14 @@ vim.api.nvim_create_user_command("LususUpdate", function()
   require("config.utils").update_all()
 end, { desc = "Updates plugins, mason packages, treesitter parsers" })
 
-vim.api.nvim_create_user_command("AstrothemeTransparentEnable", function()
+vim.api.nvim_create_user_command("AstroTransparencyOn", function()
   require("astrotheme").setup({
     style = { transparent = true },
   })
   require("config.utils").colors("astromars")
 end, { desc = "Enable astrotheme transparency" })
 
-vim.api.nvim_create_user_command("AstrothemeTransparentDisable", function()
+vim.api.nvim_create_user_command("AstroTransparencyOff", function()
   require("astrotheme").setup({
     style = { transparent = false },
   })
