1274d94a1492035c9a8d5541213366bf16c45114
Michael Berry <trismegustis@gmail.com>
2025-02-23 03:18:38 -0500

    Enhance indent-blankline config.
    Colored indents for indent-blankline can be turned off.
    indent_blankline_rainbow in custom.custom_config.enable_plugin controls
    it.

--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -3,28 +3,32 @@ local group = exist and type(custom_config) == "table" and custom_config.enable_
 local enabled = require("config.utils").enabled
 
 if enabled(group, "indent_blankline") then
-  -- configure multiple indent colors for indent-blankline
-  local highlight = {
-    "RainbowRed",
-    "RainbowGreen",
-    "RainbowOrange",
-    "RainbowBlue",
-    "RainbowYellow",
-    "RainbowViolet",
-    "RainbowCyan",
-  }
-  local hooks = require("ibl.hooks")
-  -- create the highlight groups in the highlight setup hook, so they are reset
-  -- every time the colorscheme changes
-  hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-  end)
-  -- end configuring multiple indent colors
-  require("ibl").setup({ indent = { highlight = highlight } })
+  if enabled(group, "indent_blankline_rainbow") then
+    -- configure multiple indent colors for indent-blankline
+    local highlight = {
+      "RainbowRed",
+      "RainbowGreen",
+      "RainbowOrange",
+      "RainbowBlue",
+      "RainbowYellow",
+      "RainbowViolet",
+      "RainbowCyan",
+    }
+    local hooks = require("ibl.hooks")
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    end)
+    -- end configuring multiple indent colors
+    require("ibl").setup({ indent = { highlight = highlight } })
+  else
+    require("ibl").setup()
+  end
 end
