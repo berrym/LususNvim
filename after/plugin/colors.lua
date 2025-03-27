b6b21724c5cc135b5b061c6693f00583ec89ce1a
Michael Berry <trismegustis@gmail.com>
2025-03-27 00:52:06 -0400

    Fix: Default theme can be disabled.

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,2 +1,8 @@
-local colors = require("config.utils").colors
-colors("astromars")
+local exist, custom_config = pcall(require, "custom.custom_config")
+local group = exist and type(custom_config) == "table" and custom_config.enable_plugins or {}
+local enabled = require("config.utils").enabled
+
+if enabled(group, "astrotheme") then
+  local colors = require("config.utils").colors
+  colors("astromars")
+end
